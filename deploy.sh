cf66e090d7a54fdd4b9db5db95b0f17323a57f52
aronerben <aron.erben@gmail.com>
2020-04-26 20:53:48 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack exec aronwith1a clean
+stack exec aronwith1a build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish yeet yeet"
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
