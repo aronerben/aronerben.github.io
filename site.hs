202874e8cfce2f0fffc966658c0f191150c7a9c3
aronerben <aron.erben@gmail.com>
2021-01-30 19:10:00 +0100

    Setup bloggy

--- site.hs
+++ site.hs
@@ -14,40 +14,48 @@ main =
     match "css/*" $ do
       route idRoute
       compile compressCssCompiler
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-      route $ setExtension "html"
-      compile $
-        pandocCompiler >>=
-        loadAndApplyTemplate "templates/default.html" defaultContext >>=
-        relativizeUrls
     match "posts/*" $ do
       route $ setExtension "html"
       compile $
-        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx >>=
-        loadAndApplyTemplate "templates/default.html" postCtx >>=
-        relativizeUrls
-    create ["archive.html"] $ do
-      route idRoute
-      compile $ do
-        posts <- recentFirst =<< loadAll "posts/*"
-        let archiveCtx =
-              listField "posts" postCtx (return posts) `mappend`
-              constField "title" "Archives" `mappend`
-              defaultContext
-        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
-          loadAndApplyTemplate "templates/default.html" archiveCtx >>=
-          relativizeUrls
+        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx
+          >>= loadAndApplyTemplate "templates/base.html" postCtx
+          >>= relativizeUrls
     match "index.html" $ do
       route idRoute
       compile $ do
+        let layoutCtx =
+              constField "title" "aronwith1a"
+                `mappend` constField "blogClass" ""
+                `mappend` constField "aboutClass" ""
+                `mappend` defaultContext
+        getResourceBody >>= applyAsTemplate defaultContext
+          >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+          >>= relativizeUrls
+    match "blog.html" $ do
+      route idRoute
+      compile $ do
+        let layoutCtx =
+              constField "title" "blog"
+                `mappend` constField "blogClass" "underline"
+                `mappend` constField "aboutClass" ""
+                `mappend` defaultContext
         posts <- recentFirst =<< loadAll "posts/*"
-        let indexCtx =
-              listField "posts" postCtx (return posts) `mappend`
-              constField "title" "Hometest" `mappend`
-              defaultContext
-        getResourceBody >>= applyAsTemplate indexCtx >>=
-          loadAndApplyTemplate "templates/default.html" indexCtx >>=
-          relativizeUrls
+        let blogCtx =
+              listField "posts" postCtx (return posts)
+                `mappend` defaultContext
+        getResourceBody >>= applyAsTemplate blogCtx
+          >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+          >>= relativizeUrls
+    match "about.html" $ do
+      route idRoute
+      compile $ do
+        let layoutCtx =
+              constField "title" "about"
+                `mappend` constField "blogClass" ""
+                `mappend` constField "aboutClass" "underline"
+                `mappend` defaultContext
+        getResourceBody >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+          >>= relativizeUrls
     match "templates/*" $ compile templateCompiler
 
 config :: Configuration
@@ -55,4 +63,8 @@ config = defaultConfiguration {deployCommand = "./deploy.sh"}
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
+postCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` constField "blogClass" "underline"
+    `mappend` constField "aboutClass" ""
+    `mappend` defaultContext
