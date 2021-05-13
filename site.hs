d01fd8d833adc695a174ab254e6401cdc295f712
aronerben <aron.erben@gmail.com>
2021-05-13 23:54:55 +0200

    Integrate Literate Agda support

--- site.hs
+++ site.hs
@@ -1,71 +1,155 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 
+import Control.Monad
+import Data.List (
+  isInfixOf,
+  isSuffixOf,
+ )
 import Data.Monoid (mappend)
+import Data.String
+import Debug.Trace
+import qualified GHC.IO.Encoding as E
 import Hakyll
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Process
 
-
---------------------------------------------------------------------------------
 main :: IO ()
-main =
-  hakyllWith config $ do
-    match ("images/*" .||. "favicon.ico" .||. "CNAME") $ do
-      route idRoute
-      compile copyFileCompiler
-    match "css/*" $ do
-      route idRoute
-      compile compressCssCompiler
-    match "posts/*" $ do
-      route $ setExtension "html"
-      compile $
-        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx
-          >>= loadAndApplyTemplate "templates/base.html" postCtx
-          >>= relativizeUrls
-    match "index.html" $ do
-      route idRoute
-      compile $ do
-        let layoutCtx =
-              constField "title" "aronwith1a"
-                `mappend` constField "blogClass" ""
-                `mappend` constField "aboutClass" ""
-                `mappend` defaultContext
-        getResourceBody >>= applyAsTemplate defaultContext
-          >>= loadAndApplyTemplate "templates/base.html" layoutCtx
-          >>= relativizeUrls
-    match "blog.html" $ do
-      route idRoute
-      compile $ do
-        let layoutCtx =
-              constField "title" "blog"
-                `mappend` constField "blogClass" "underline"
-                `mappend` constField "aboutClass" ""
-                `mappend` defaultContext
-        posts <- recentFirst =<< loadAll "posts/*"
-        let blogCtx =
-              listField "posts" postCtx (return posts)
-                `mappend` defaultContext
-        getResourceBody >>= applyAsTemplate blogCtx
-          >>= loadAndApplyTemplate "templates/base.html" layoutCtx
-          >>= relativizeUrls
-    match "about.html" $ do
-      route idRoute
-      compile $ do
-        let layoutCtx =
-              constField "title" "about"
-                `mappend` constField "blogClass" ""
-                `mappend` constField "aboutClass" "underline"
-                `mappend` defaultContext
-        getResourceBody >>= loadAndApplyTemplate "templates/base.html" layoutCtx
-          >>= relativizeUrls
-    match "templates/*" $ compile templateCompiler
+main = do
+  E.setLocaleEncoding E.utf8
+  hakyllMain
+
+data Point = Point
+  { pointX, pointY :: Double
+  , pointName :: String
+  }
+  deriving (Show)
+
+hakyllMain :: IO ()
+hakyllMain = hakyllWith config $ do
+  match ("images/*" .||. "favicon.ico" .||. "CNAME") $ do
+    route idRoute
+    compile copyFileCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/base.html" postCtx
+        >>= loadAndApplyTemplate "templates/base.html" postCtx
+        >>= relativizeUrls
+  match (fromString $ agdaOutputDir </> "*.md") $ do
+    route $
+      setExtension "html"
+        `composeRoutes` gsubRoute
+          (agdaOutputDir </> "")
+          (const "posts")
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/base.html" postCtx
+        >>= relativizeUrls
+  match (fromString $ agdaOutputDir </> "*.css") $ do
+    route $ gsubRoute (agdaOutputDir </> "") (const "posts")
+    compile compressCssCompiler
+  match (fromString $ agdaOutputDir </> "*.html") $ do
+    route $ gsubRoute (agdaOutputDir </> "") (const "posts")
+    compile copyFileCompiler
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      let layoutCtx =
+            constField "title" "aronwith1a"
+              `mappend` constField "blogClass" ""
+              `mappend` constField "aboutClass" ""
+              `mappend` defaultContext
+      getResourceBody
+        >>= applyAsTemplate defaultContext
+        >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+        >>= relativizeUrls
+  match "blog.html" $ do
+    route idRoute
+    compile $ do
+      let layoutCtx =
+            constField "title" "blog"
+              `mappend` constField "blogClass" "underline"
+              `mappend` constField "aboutClass" ""
+              `mappend` defaultContext
+      plainPosts <- loadAll "posts/*"
+      agdaPosts <- loadAll $ fromString (agdaOutputDir </> "*.md")
+      posts <- recentFirst $ plainPosts ++ agdaPosts
+      let blogCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` defaultContext
+      getResourceBody
+        >>= applyAsTemplate blogCtx
+        >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+        >>= relativizeUrls
+  match "about.html" $ do
+    route idRoute
+    compile $ do
+      let layoutCtx =
+            constField "title" "about"
+              `mappend` constField "blogClass" ""
+              `mappend` constField "aboutClass" "underline"
+              `mappend` defaultContext
+      getResourceBody
+        >>= loadAndApplyTemplate "templates/base.html" layoutCtx
+        >>= relativizeUrls
+  match "templates/*" $ compile templateCompiler
+  preprocess processAgdaPosts
 
 config :: Configuration
 config = defaultConfiguration {deployCommand = "./deploy.sh"}
 
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
   dateField "date" "%B %e, %Y"
     `mappend` constField "blogClass" "underline"
     `mappend` constField "aboutClass" ""
     `mappend` defaultContext
+
+-- Agda stuff from https://jesper.sikanda.be/posts/literate-agda.html
+agdaCommand :: String
+agdaCommand = "agda"
+
+agdaInputDir :: String
+agdaInputDir = "agda-posts"
+
+agdaOutputDir :: String
+agdaOutputDir = "_agda"
+
+agdaOptions :: String -> [String]
+agdaOptions fileName =
+  [ "--html"
+  , "--html-highlight=auto"
+  , "--html-dir=" ++ agdaOutputDir
+  , "-i" ++ agdaInputDir
+  , agdaInputDir </> fileName
+  ]
+
+-- Process a .lagda.md file into markdown by calling Agda
+processAgdaPosts :: IO ()
+processAgdaPosts = do
+  files <- listDirectory agdaInputDir
+  let agdaFiles = filter (".lagda.md" `isSuffixOf`) files
+  forM_ agdaFiles processAgdaPost
+
+processAgdaPost :: FilePath -> IO ()
+processAgdaPost agdaFile = do
+  exitCode <-
+    readProcessWithExitCode
+      agdaCommand
+      (agdaOptions agdaFile)
+      mempty
+  case exitCode of
+    (ExitFailure _, err, _) -> do
+      putStrLn $ "Failed to process " ++ agdaFile
+      putStrLn err
+    (ExitSuccess, out, _) -> do
+      putStrLn out
